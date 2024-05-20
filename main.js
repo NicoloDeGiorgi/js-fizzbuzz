a80e00460a87f5c71235db1ddc55ceeacb2e8c50
Nicolò <nico.2699@icloud.com>
2024-05-20 17:20:50 +0200

    add: error-correcting

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 'use strict'
 
-for (let i = 1; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
     //multipli di 3 che di 5 stampi 'FizzBuzz'
-    if (i % 15 == 0) console.log("FizzBuzz");
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
     // se multipli di 3 stampo 'Fizz'
-    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
     // se multipli di 3 stampo 'Buzz'
-    else if (i % 5 == 0) console.log("Buzz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
     //altrimenti stampo il valore di 'i'
     else console.log(i);
 
